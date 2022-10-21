127f4774438306f0f0843cc6920a7e480315f6c2
peter91v <53484365+peter91v@users.noreply.github.com>
2022-10-21 14:53:45 +0200

    Update plantinstall.sh
    edited structure

--- plantinstall.sh
+++ plantinstall.sh
@@ -16,38 +16,38 @@ if [ "$CONT" = "y" ]; then
 	sudo apt-get install libreadline-dev  
 	sudo apt-get install libffi-dev
 	sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev
-	cd /home/pi/Plant_Care
+    	sudo mysql_secure_installation
+  	sudo apt install mosquitto mosquitto-clients
+	sudo apt install mariadb-server
+	sudo apt-get install libmariadbclient-dev
+	cd ~/Plant_Care
         mkdir temp
         cd temp
         wget https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
         tar xfz Python-3.8.6.tgz
         cd Python-3.8.6
-        ./configure --prefix=/home/pi/Plant_Care/Python-3.8.6
+        ./configure --prefix=~/Plant_Care/Python-3.8.6
         make -j4
         make install
     fi
-    cd /home/pi/Plant_Care/Python-3.8.6
+    cd ~/Plant_Care/Python-3.8.6
     cd bin
     ./python3 -m pip install --upgrade pip
     ./python3 -m venv ~/Plant_Care/Zope
-    cd /home/pi/Plant_Care/Zope
+    cd ~/Plant_Care/Zope
     bin/pip install zc.buildout
     bin/pip install wheel
-    cp /home/pi/Plant_Care/buildout.cfg /home/pi/Plant_Care/Zope
+    bin/pip3 install adafruit-circuitpython-mcp3xxx
+    bin/pip3 install RPi.gpio
+    bin/pip3 install mysql-connector-python==8.0.29
+    bin/pip3 install paho-mqtt
+    cp ~/Plant_Care/buildout.cfg ~/Plant_Care/Zope
     mkdir src
     cd src
     git clone https://github.com/peter91v/PlantCare.git .
    
     cd ..
     bin/buildout
-    sudo apt install mariadb-server
-    sudo apt-get install libmariadbclient-dev
-    bin/pip3 install adafruit-circuitpython-mcp3xxx
-    bin/pip3 install RPi.gpio
-    bin/pip3 install mysql-connector-python==8.0.29
-    bin/pip3 install paho-mqtt
-    sudo mysql_secure_installation
-    sudo apt install mosquitto mosquitto-clients
     exec bash
 elsesvn up
     echo "cancel install Python / Plone"
