ec69bb52822299d676a67bcaa1c5194077a7915a
peter91v <53484365+peter91v@users.noreply.github.com>
2022-10-21 14:48:06 +0200

    Update plantinstall.sh
    add new packages

--- plantinstall.sh
+++ plantinstall.sh
@@ -13,8 +13,9 @@ if [ "$CONT" = "y" ]; then
 	sudo apt-get install openssl
 	sudo apt-get install libssl-dev
 	sudo apt-get install libbz2-dev
-	sudo apt-get install readline-dev  
+	sudo apt-get install libreadline-dev  
 	sudo apt-get install libffi-dev
+	sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev
 	cd /home/pi/Plant_Care
         mkdir temp
         cd temp
